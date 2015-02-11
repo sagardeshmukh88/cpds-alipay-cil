2387d2986c57a1d9141cf6b53d5a08458d819237
unknown <Sagar.Deshmukh@SYNPUNEN580104D.synechron.com>
2015-02-11 17:26:55 +0530

    Adding Source folders to the sub modules

---
+++ cpds-common/cpds-authorization/src/main/java/com/veritranspay/cpds/common/authorization/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.veritranspay.cpds.common.authorization;
+
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+
+public class HelloWorld {
+//	private static final Logger logger = LogManager.getLogger(HelloWorld.class);
+
+	public static void main(String[] args) {
+//		logger.info("Hello, World!");
+//		logger.debug("debug");
+//		logger.error("error");
+//		logger.trace("trace");
+	}
+}
